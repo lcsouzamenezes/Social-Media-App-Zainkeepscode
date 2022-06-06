6f118f24b1b0bf4dcde2590e015fb89d2a75337d
ZainRk <ahmadrao925@gmail.com>
2022-06-06 09:45:07 +0500

    Middleware improved

--- src/actions/UserAction.js
+++ src/actions/UserAction.js
@@ -4,9 +4,8 @@ import * as UserApi from "../api/UserRequests";
 export const updateUser=(id, formData)=> async(dispatch)=> {
     dispatch({type: "UPDATING_START"})
     try{
-        console.log("user ka action start ho gya")
-        console.log("Action ka data", formData)
         const {data} = await UserApi.updateUser(id, formData);
+        console.log("Action ko receive hoa hy ye : ",data)
         dispatch({type: "UPDATING_SUCCESS", data: data})
     }   
     catch(error){
